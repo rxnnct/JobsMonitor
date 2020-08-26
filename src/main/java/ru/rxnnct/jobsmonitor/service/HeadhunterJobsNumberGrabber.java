27a5dadfb565f3f4ce76b05c118f63bee2b7a6cc
Rixen <rixennoct@rambler.ru>
2020-08-26 19:53:08 +0300

    Scheduler added.

---
+++ src/main/java/ru/rxnnct/jobsmonitor/service/HeadhunterJobsNumberGrabber.java
@@ -0,0 +1,20 @@
+package ru.rxnnct.jobsmonitor.service;
+
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
+@EnableScheduling
+public class HeadhunterJobsNumberGrabber {
+
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+    @Scheduled(cron = "0 */1 * ? * *")
+    public void reportCurrentTime() {
+        System.out.println(dateFormat.format(new Date()) + ": Test!");
+    }
+}
