b8a8ef422fb7d0a9a3c053889514a10deef728c1
Rixen <rixennoct@rambler.ru>
2020-08-27 02:56:51 +0300

    Exception package new name.

--- src/main/java/ru/rxnnct/jobsmonitor/exception/NotFoundException.java
+++ src/main/java/ru/rxnnct/jobsmonitor/exception/NotFoundException.java
@@ -1,4 +1,4 @@
-package ru.rxnnct.jobsmonitor.exceptions;
+package ru.rxnnct.jobsmonitor.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
