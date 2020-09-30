820c2576b0825b993a1a0c0822a4edde5f8eda7f
Rixen <rixennoct@rambler.ru>
2020-09-30 16:42:12 +0300

    Getting url in scheduler.

--- src/main/java/ru/rxnnct/jobsmonitor/service/HeadhunterJobsNumberGrabber.java
+++ src/main/java/ru/rxnnct/jobsmonitor/service/HeadhunterJobsNumberGrabber.java
@@ -1,19 +1,35 @@
 package ru.rxnnct.jobsmonitor.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import ru.rxnnct.jobsmonitor.domain.SourceGetMethod;
+import ru.rxnnct.jobsmonitor.repo.SourceGetMethodRepo;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.List;
 
 @Component
 @EnableScheduling
 public class HeadhunterJobsNumberGrabber {
-    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+    private final SourceGetMethodRepo sourceGetMethodRepo;
+
+    @Autowired
+    public HeadhunterJobsNumberGrabber(SourceGetMethodRepo sourceGetMethodRepo) {
+        this.sourceGetMethodRepo = sourceGetMethodRepo;
+    }
 
     @Scheduled(cron = "${headhunterJobsNumberGrabberSchedulerCronExpression}")
     public void reportCurrentTime() {
-        System.out.println(dateFormat.format(new Date()) + ": Test!");
+        List<SourceGetMethod> sourceGetMethods;
+        sourceGetMethods = sourceGetMethodRepo.findAll();
+        sourceGetMethods.forEach(sourceGetMethod -> {
+            String currentUrl;
+            currentUrl = sourceGetMethod.getUrl();
+            System.out.println(currentUrl);
+        });
     }
+
+
 }
