f9b1738b865347e1e06ea789096a359ed75b5159
Rixen <rixennoct@rambler.ru>
2020-10-12 14:42:50 +0300

    Oauth2 removed.

--- src/main/java/ru/rxnnct/jobsmonitor/config/WebSecurityConfig.java
+++ src/main/java/ru/rxnnct/jobsmonitor/config/WebSecurityConfig.java
@@ -1,54 +1,42 @@
 package ru.rxnnct.jobsmonitor.config;
 
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.PrincipalExtractor;
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import ru.rxnnct.jobsmonitor.domain.Role;
-import ru.rxnnct.jobsmonitor.domain.User;
-import ru.rxnnct.jobsmonitor.repo.UserDetailsRepo;
-
-import java.util.Collections;
 
 @Configuration
-@EnableWebSecurity
-@EnableOAuth2Sso
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Value("${adminPanelUsername}")
+    String adminPanelUsername;
+    @Value("${adminPanelPassword}")
+    String adminPanelPassword;
+
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    protected void configure(HttpSecurity http) throws Exception
+    {
         http
+                .csrf().disable()
                 .authorizeRequests()
-                .mvcMatchers("/").permitAll()
-                //todo: fix it
-//                .mvcMatchers("/admin*").hasRole("ADMIN")
-                //todo: make the same for API
-                .anyRequest().authenticated()
+                .antMatchers("/").permitAll()
+                .antMatchers("/admin**").hasRole("ADMIN")
+                .antMatchers("/api/admin/").hasRole("ADMIN")
+                .anyRequest()
+                .authenticated()
                 .and()
-                .csrf().disable();
+                .httpBasic();
     }
 
-    @Bean
-    public PrincipalExtractor principalExtractor(UserDetailsRepo userDetailsRepo) {
-        return map -> {
-            String id = (String) map.get("sub");
-
-            User user = userDetailsRepo.findById(id).orElseGet(() -> {
-                User newUser = new User();
-
-                newUser.setId(id);
-                newUser.setName((String) map.get("name"));
-                newUser.setEmail((String) map.get("email"));
-                newUser.setGender((String) map.get("gender"));
-                newUser.setLocale((String) map.get("locale"));
-                newUser.setUserpic((String) map.get("picture"));
-                newUser.setRoles(Collections.singleton(Role.USER));
-                return newUser;
-            });
-
-            return userDetailsRepo.save(user);
-        };
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth)
+            throws Exception
+    {
+        auth.inMemoryAuthentication()
+                .withUser(adminPanelUsername)
+                .password("{noop}" + adminPanelPassword)
+                .roles("ADMIN");
     }
+
 }
--- src/main/java/ru/rxnnct/jobsmonitor/repo/UserDetailsRepo.java
+++
@@ -1,7 +0,0 @@
-package ru.rxnnct.jobsmonitor.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.rxnnct.jobsmonitor.domain.User;
-
-public interface UserDetailsRepo extends JpaRepository<User, String> {
-}
