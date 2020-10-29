95bba2960ad25accc5fcf662931736801e7ce0b1
Rixen <rixennoct@rambler.ru>
2020-10-29 23:53:34 +0300

    JobsQtyConverter.

--- src/main/java/ru/rxnnct/jobsmonitor/controller/JobsQtyController.java
+++ src/main/java/ru/rxnnct/jobsmonitor/controller/JobsQtyController.java
@@ -1,12 +1,14 @@
 package ru.rxnnct.jobsmonitor.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.rxnnct.jobsmonitor.domain.JobsQty;
 import ru.rxnnct.jobsmonitor.repo.JobsQtyRepo;
+import ru.rxnnct.jobsmonitor.service.JobsQtyConverter;
 
 import java.util.List;
 
@@ -14,10 +16,12 @@ import java.util.List;
 @RequestMapping("api/jobs-qty")
 public class JobsQtyController {
     private final JobsQtyRepo jobsQtyRepo;
+    private final ApplicationContext context;
 
     @Autowired
-    public JobsQtyController(JobsQtyRepo jobsQtyRepo) {
+    public JobsQtyController(JobsQtyRepo jobsQtyRepo, ApplicationContext context) {
         this.jobsQtyRepo = jobsQtyRepo;
+        this.context = context;
     }
 
 //    @GetMapping
@@ -28,7 +32,8 @@ public class JobsQtyController {
     //test
     @GetMapping
     public String stats() {
-        return "Data_Java_Python,2001_1000_400,2002_1300_500";
+        return context.getBean(JobsQtyConverter.class).convertToGoogleChartsArray(jobsQtyRepo.findAll());
+//        return "Data_Java_Python,2001_1000_400,2002_1300_500";
     }
 
 //    @GetMapping("{id}")
