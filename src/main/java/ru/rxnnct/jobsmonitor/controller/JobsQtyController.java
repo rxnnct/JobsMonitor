b51b34b6b0d9687edae0d9537604ba441a3bfbe3
Rixen <rixennoct@rambler.ru>
2020-10-30 00:19:05 +0300

    JobsQtyController comments removed.

--- src/main/java/ru/rxnnct/jobsmonitor/controller/JobsQtyController.java
+++ src/main/java/ru/rxnnct/jobsmonitor/controller/JobsQtyController.java
@@ -3,14 +3,10 @@ package ru.rxnnct.jobsmonitor.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.rxnnct.jobsmonitor.domain.JobsQty;
 import ru.rxnnct.jobsmonitor.repo.JobsQtyRepo;
 import ru.rxnnct.jobsmonitor.service.JobsQtyConverter;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("api/jobs-qty")
@@ -24,20 +20,8 @@ public class JobsQtyController {
         this.context = context;
     }
 
-//    @GetMapping
-//    public List<JobsQty> list() {
-//        return jobsQtyRepo.findAll();
-//    }
-
-    //test
     @GetMapping
     public String stats() {
         return context.getBean(JobsQtyConverter.class).convertToGoogleChartsArray(jobsQtyRepo.findAll());
-//        return "Data_Java_Python,2001_1000_400,2002_1300_500";
     }
-
-//    @GetMapping("{id}")
-//    public JobsQty getOne(@PathVariable("id") JobsQty jobsQty) {
-//        return jobsQty;
-//    }
 }
